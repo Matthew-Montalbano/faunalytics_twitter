0eebe9f087f4f2779fc3c2d27c6eafaffe4b0e5e
Ubuntu <ubuntu@ip-172-31-57-231.ec2.internal>
2019-03-04 03:04:21 +0000

    small bug fix re filepath

--- zip_todays_data.sh
+++ zip_todays_data.sh
@@ -7,4 +7,4 @@ mkdir "../data/all_data_${DATE}"
 mv ../data/tweets*${DATE}*.pkl ../data/all_data_${DATE}
 
 zip -r ../data/all_data_${DATE}.zip ../data/all_data_${DATE}
-rm -R data/all_data_${DATE}
+rm -R ../data/all_data_${DATE}
