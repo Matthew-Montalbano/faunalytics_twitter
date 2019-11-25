22a253c621fe3f7c41f69530ddc8d51a78dfbd7d
Ubuntu <ubuntu@ip-172-31-57-231.ec2.internal>
2019-11-25 17:41:02 +0000

    added line to backup raw pkl to s3

--- zip_todays_data.sh
+++ zip_todays_data.sh
@@ -8,3 +8,6 @@ mv ../data/tweets*${DATE}*.pkl ../data/all_data_${DATE}
 
 zip -r ../data/all_data_${DATE}.zip ../data/all_data_${DATE}
 rm -R ../data/all_data_${DATE}
+
+aws s3 cp ../data/all_data_${DATE}.zip s3://faunalytics-twitter/raw_data_pkl/
+
