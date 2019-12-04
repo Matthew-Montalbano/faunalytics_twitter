1acddbf4e20fa4dd22edac01f348b9c9bbd277a7
Stingray-M <mmatt246@gmail.com>
2019-12-03 20:02:45 -0500

    code cleanup and retweet text fix

--- read_tweets_from_files.py
+++ read_tweets_from_files.py
@@ -1,39 +1,59 @@
 import os
-import pandas as pd
+import re
 import pickle as pkl
+import pandas as pd
 
-pkl_files = os.listdir("temp_data/all_data_2019-05-16")
+def get_file_category(filename):
+    match = re.search(r'tweets_(\w*-?\w*)_', filename)
+    return match.group(1)
 
-ids = []
-txts = []
-uids = []
-favs = []
-rets = []
-times = []
 
-for pf in pkl_files:
-    print("Processing", pf)
-    with open('temp_data/all_data_2019-05-16/%s' % pf, 'rb') as f:
-    #with open('temp_data/all_data_2019-05-16/tweets_clean_meat_2019-05-16_128.pkl', 'rb') as f:
-        raw_data = pkl.load(f)
+def get_tweet_text(data):
+    try:
+        text = 'RT @{0} {1}'.format(data['retweeted_status']['user']['screen_name'],
+                                    data['retweeted_status']['full_text'])
+    except KeyError:
+        text = data['full_text']
+    return text
 
-    for x in raw_data:
-        # print(x)
-        ids += [x['id']]
-        txts += [x['full_text']]
-        uids += [x['user']['id']]
-        favs += [x['favorite_count']]
-        rets += [x['retweet_count']]
-        times += [x['created_at']]
 
-clean_data = pd.DataFrame({'id': ids,
-                           'text': txts,
-                           'user_id': uids,
-                           'favorite_count': favs,
-                           'retweet_count': rets,
-                           'date_time': times
-                           })
+if __name__ == "__main__":
+    #PKL_FILES = os.listdir("temp_data/all_data_2019-05-16")
+    PKL_FILES = os.listdir("../data")
 
-print(clean_data)
+    ids = []
+    txts = []
+    uids = []
+    favs = []
+    rets = []
+    times = []
+    categories = []
 
-clean_data.to_csv("test123.csv", index=False, encoding='utf-8')
+    for pf in PKL_FILES:
+        print("Processing", pf)
+        category = get_file_category(pf)
+        #with open('temp_data/all_data_2019-05-16/%s' % pf, 'rb') as f:
+        #with open('temp_data/all_data_2019-05-16/tweets_clean_meat_2019-05-16_128.pkl', 'rb') as f:
+        with open("../data/%s" % pf, 'rb') as f:
+            raw_data = pkl.load(f)
+
+        for x in raw_data:
+            ids += [x['id']]
+            txts += [get_tweet_text(x)]
+            uids += [x['user']['id']]
+            favs += [x['favorite_count']]
+            rets += [x['retweet_count']]
+            times += [x['created_at']]
+            categories += [category]
+
+    clean_data = pd.DataFrame({'id': ids,
+                               'text': txts,
+                               'user_id': uids,
+                               'favorite_count': favs,
+                               'retweet_count': rets,
+                               'date_time': times,
+                               'category': categories
+                               })
+
+    print(clean_data)
+    clean_data.to_csv("test123.csv", index=False, encoding='utf-8')
