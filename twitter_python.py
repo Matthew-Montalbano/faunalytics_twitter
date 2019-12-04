df788e4069443173f5559c67960ca48687a52e89
Stingray-M <mmatt246@gmail.com>
2019-12-03 20:01:02 -0500

    code cleanup

--- twitter_python.py
+++ twitter_python.py
@@ -2,126 +2,23 @@
 # Automatically search for terms without spaces as well!
 
 # import numpy as np
-# pip install python-twitter
-# import twitter
-import tweepy
-# import csv
 import datetime
-# import jsonpickle
 import pickle
 import re
 import sys
+import tweepy
+# import csv
+# import jsonpickle
 
-consumer_key = "PDZPPLGUu5ySTrR5viqi2l9mN"
-consumer_secret = "9VOIBWCGl7Y7sJ6hXaP5QY9j7wKYBMo9NzK8Fun29IIpRc1Xbo"
-access_token = "796525448301780992-ef1dlKykooXAaiD9RWQS87FdqK8cMnF"
-access_token_secret = "tXT82ibd014C9xfUk7kBaIcZdBzm8D7cf999rD2gISl3L"
+'''
+Paul's keys:
+CONSUMER_KEY = "PDZPPLGUu5ySTrR5viqi2l9mN"
+CONSUMER_SECRET = "9VOIBWCGl7Y7sJ6hXaP5QY9j7wKYBMo9NzK8Fun29IIpRc1Xbo"
+ACCESS_TOKEN = "796525448301780992-ef1dlKykooXAaiD9RWQS87FdqK8cMnF"
+ACCESS_TOKEN_SECRET = "tXT82ibd014C9xfUk7kBaIcZdBzm8D7cf999rD2gISl3L"
+'''
 
-max_tweets = 1000000
-
-
-def download_twitter_search(search_terms, search_group, offset):
-
-    # https://gist.github.com/vickyqian/f70e9ab3910c7c290d9d715491cde44c
-    #  Good info!:
-    # http://www.dealingdata.net/2016/07/23/PoGo-Series-Tweepy/
-
-    # auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    # auth.set_access_token(access_token, access_token_secret)
-
-    # api = tweepy.API(auth,wait_on_rate_limit=True)
-
-    # Switching to application authentication
-    auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
-
-    # Setting up new api wrapper, using authentication only
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-    # Error handling
-    if (not api):
-        print("Problem Connecting to API")
-
-    start_date = (datetime.datetime.today() + datetime.timedelta(days=-offset)).strftime('%Y-%m-%d')
-    end_date = (datetime.datetime.today() + datetime.timedelta(days=(-offset + 1))).strftime('%Y-%m-%d')
-
-    # Open/Create a file to append data
-    # csvFile = open('test_vegan.csv', 'a')
-    # Use csv Writer
-    # csvWriter = csv.writer(csvFile)
-    # json_file = open('test_vegan_%s.json'%start_date, 'w')
-
-    print(start_date)
-    print(end_date)
-
-    print(api.rate_limit_status()['resources']['search'])
-
-    search_query = ""
-    for term in search_terms:
-        if len(search_query) > 0:
-            search_query += ' OR '
-        # search_query += '"' + term + '"'
-        search_query += '"{0}"'.format(term)
-        if re.search(' ', term):
-            # search_query += ' OR "' + re.sub(' ', '', term) + '"'
-            search_query += ' OR "{0}"'.format(re.sub(' ', '', term))
-
-    print(search_query)
-
-    # cursor version
-    # tweets = tweepy.Cursor(api.search,q=search_query,count=100,
-    #                                                   lang="en",
-    #                                                   since=start_date,
-    #                           until=end_date,tweet_mode = "extended").items(max_tweets)
-
-    output_dict = []
-
-    max_id = -1
-    tweet_count = 0
-    # page = 1
-    while tweet_count < max_tweets:
-        if max_id <= 0:
-            tweets = api.search(q=search_query,
-                                count=100,
-                                lang="en",
-                                since=start_date,
-                                until=end_date,
-                                tweet_mode="extended")
-        else:
-            tweets = api.search(q=search_query,
-                                count=100,
-                                lang="en",
-                                since=start_date,
-                                until=end_date,
-                                tweet_mode="extended",
-                                max_id=str(max_id - 1))
-
-        if tweets:
-            for tweet in tweets:
-                # print (tweet._json['created_at'])
-                output_dict += [tweet._json]
-                tweet_count += 1
-                # csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
-
-                if tweet_count > 0 and tweet_count % 10000 == 0:
-                    with open('../data/tweets_%s_%s_%d.pkl' % (search_group, start_date, tweet_count), 'wb') as handle:
-                        pickle.dump(output_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-                    print('Written %d tweets.' % tweet_count)
-                    print(tweet._json['created_at'])
-                    print(tweets[-1].id)
-                    output_dict = []
-
-            max_id = tweets[-1].id
-        else:
-            break
-
-    with open('../data/tweets_%s_%s_%d.pkl' % (search_group, start_date,tweet_count), 'wb') as handle:
-            pickle.dump(output_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    print('Written %d tweets.' % tweet_count)
-
-    print("Downloaded {0} tweets".format(tweet_count))
-
-
-search_terms_lists = {
+SEARCH_TERMS_LISTS = {
     'vegan':
     [
         "vegan",
@@ -193,14 +90,109 @@ search_terms_lists = {
     ]
 }
 
+CONSUMER_KEY = "4L3PcDJIZR1TTD9J6TRnlqFxH"
+CONSUMER_SECRET = "39GEwYjAcGdcfJqM3S64Jqh5iHfnM1MEuwS7fWOS9zNzYesFmk"
+ACCESS_TOKEN = "1201957817424457728-i8E1w5cHa1QhiWuYX1qgWzF6Glhrwa"
+ACCESS_TOKEN_SECRET = "aTZhJwgxLwQvz6200WW63KAaKiucuOJOEcQU1w4MBLKrN"
 
-if len(sys.argv) == 1:
-    offset = 2
-else:
-    offset = int(sys.argv[1])
+MAX_TWEETS = 1000000
 
-# Pick list of terms to search.
-for group in search_terms_lists.keys():
-# for group in ['other']:
-    print(group)
-    download_twitter_search(search_terms_lists[group], group, offset)
+def download_twitter_search(search_terms, search_group, offset):
+    api = get_twitter_api()
+    start_date, end_date = get_start_and_end_dates(offset)
+    print(api.rate_limit_status()['resources']['search'])
+    search_query = build_search_query(search_terms)
+    output_dict = []
+    max_id = -1
+    tweet_count = 0
+    # page = 1
+    while tweet_count < MAX_TWEETS:
+        if max_id <= 0:
+            tweets = api.search(q=search_query,
+                                count=100,
+                                lang="en",
+                                since=start_date,
+                                until=end_date,
+                                tweet_mode="extended")
+        else:
+            tweets = api.search(q=search_query,
+                                count=100,
+                                lang="en",
+                                since=start_date,
+                                until=end_date,
+                                tweet_mode="extended",
+                                max_id=str(max_id - 1))
+
+        if tweets:
+            for tweet in tweets:
+                output_dict += [tweet._json]
+                tweet_count += 1
+                if tweet_count > 0 and tweet_count % 10000 == 0:
+                    create_pkl_file(search_group, start_date, tweet_count, output_dict)
+                    print(tweet._json['created_at'])
+                    print(tweets[-1].id)
+                    output_dict = []
+
+            max_id = tweets[-1].id
+        else:
+            break
+
+    create_pkl_file(search_group, start_date, tweet_count, output_dict)
+    print("Downloaded {0} tweets".format(tweet_count))
+
+
+def get_twitter_api():
+    # https://gist.github.com/vickyqian/f70e9ab3910c7c290d9d715491cde44c
+    #  Good info!:
+    # http://www.dealingdata.net/2016/07/23/PoGo-Series-Tweepy/
+
+    # auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    # auth.set_ACCESS_TOKEN(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    # api = tweepy.API(auth,wait_on_rate_limit=True)
+
+    auth = tweepy.AppAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    if not api:
+        print("Problem Connecting to API")
+    return api
+
+
+def get_start_and_end_dates(offset):
+    start_date = (datetime.datetime.today() + datetime.timedelta(days=-offset)).strftime('%Y-%m-%d')
+    end_date = (datetime.datetime.today() + datetime.timedelta(days=(-offset + 1))).strftime('%Y-%m-%d')
+    print(start_date)
+    print(end_date)
+    return start_date, end_date
+
+
+def build_search_query(search_terms):
+    search_query = ""
+    for term in search_terms:
+        if len(search_query) > 0:
+            search_query += ' OR '
+        search_query += '"{0}"'.format(term)
+        if re.search(' ', term):
+            search_query += ' OR "{0}"'.format(re.sub(' ', '', term))
+    print(search_query)
+    return search_query
+
+
+def create_pkl_file(search_group, start_date, tweet_count, output):
+    file_name = "../data/tweets_{0}_{1}_{2}.pkl".format(search_group, start_date, tweet_count)
+    with open(file_name, 'wb') as handle:
+        pickle.dump(output, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print('Written %d tweets.' % tweet_count)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        OFFSET = 2
+    else:
+        OFFSET = int(sys.argv[1])
+
+    # Pick list of terms to search.
+    for group in SEARCH_TERMS_LISTS:
+    # for group in ['other']:
+        print(group)
+        download_twitter_search(SEARCH_TERMS_LISTS[group], group, OFFSET)
