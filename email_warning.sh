2692fe79d35e614dc901938e260bc63e63c8cf6e
root <root@raspberrypi>
2020-01-05 10:07:23 -0500

    small updates

--- email_warning.sh
+++ email_warning.sh
@@ -3,4 +3,4 @@ set -e
 DATE="$(date +%Y-%m-%d --date="-$1 days")"
 echo $DATE
 
-[ -f "../data/all_data_${DATE}.zip" ] || mail -s "OH GOD! something went wrong..." paulfornia@gmail.com < /dev/null
+[ -f "../data/all_data_${DATE}.zip" ] || echo "Check faunalytics twitter for date ${DATE}" | /usr/bin/mail -s "OH GOD! something went wrong..." mmatt246@gmail.com
